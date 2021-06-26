44615533cf5352d9492268608c0d2e54972796e4
Pedro-Augusto-Barbosa-Aparecido <pedr.augustobarbosa.aparecido.gmail.com>
2021-06-26 11:57:52 -0300

    Fix Slab generation

--- utils/generate_data.py
+++ utils/generate_data.py
@@ -4,7 +4,8 @@ import string
 
 def generate_slab_number(size=None, initial_format=None):
     if size and initial_format:
-        return f'{initial_format} - '.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+        slab = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+        return f'{initial_format}{slab}'
     elif size and not initial_format:
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
     else:
